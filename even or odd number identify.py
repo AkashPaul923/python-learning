26ad84aa49f3c2e566de0b7b29da6b3186e94df2
Akash Paul <152735946+AkashPaul923@users.noreply.github.com>
2024-02-28 00:14:00 +0600

    first code

--- even or odd number identify.py
+++ even or odd number identify.py
@@ -1,6 +1,11 @@
 #even or odd number identify
+
+
+#input
 x=int(input("Enter a number :"))
+
+#condition check and print massage
 if (x%2)==0:
-    print(x ,"is Even",)
+    print(x ,"is Even Number",)
 else:
-    print(x, "is ODD")
+    print(x, "is Odd Number")
