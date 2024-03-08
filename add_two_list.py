9b93c0b2ccf173fef448cbc280e179be0c364032
Akash Paul <152735946+AkashPaul923@users.noreply.github.com>
2024-03-09 00:07:13 +0600

    add two list

---
+++ add_two_list.py
@@ -0,0 +1,12 @@
+list1 = ['A','B','C']
+list2 = [1,2,3]
+print('List1 ',list1)
+print('List2 ',list2)
+
+#add two list using '+' operator
+list3 = list1 + list2
+print(list3)
+
+#add two list using extend() function
+list1.extend(list2)
+print(list1)
