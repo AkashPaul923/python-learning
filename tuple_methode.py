4d29c470d1a6e1b357895f6b852b12ede80d0749
Akash Paul <152735946+AkashPaul923@users.noreply.github.com>
2024-03-11 00:19:14 +0600

    count and index methode of tuple

---
+++ tuple_methode.py
@@ -0,0 +1,9 @@
+a = (1,2,5,6,5,1,4,4,1,9,3,5)
+
+#count a number in tuple
+x = a.count(5)
+print("number is in tuple ", x,"times")
+
+#find index of a number in a tuple
+y = a.index(9)
+print("the index of given number ",y)
