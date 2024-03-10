70ad9bff57ea2ffdda78ebc881de1d01f33f4db5
Akash Paul <152735946+AkashPaul923@users.noreply.github.com>
2024-03-11 00:20:59 +0600

    tuple joining and multipling

---
+++ join_tuple.py
@@ -0,0 +1,14 @@
+#join tuple
+a = ("Apple" , "Banana" , "Cheery" )
+b = ("Dog" , "Lion" , "tiger" , "Deer")
+print(a)
+print(b)
+
+c = a + b
+print(c)
+
+#multiply tuple
+d = a * 2
+e = b * 3
+print(d)
+print(e)
