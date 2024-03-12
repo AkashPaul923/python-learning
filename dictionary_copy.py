83911fa8bc3a780c28d30000f50deddfa4427a64
Akash Paul <152735946+AkashPaul923@users.noreply.github.com>
2024-03-13 00:12:18 +0600

    copy a dictionary into another

---
+++ dictionary_copy.py
@@ -0,0 +1,23 @@
+studentinfo = {
+    'Name': 'Akash Paul',
+    'Roll': 21078,
+    'Dept': 'CSE',
+    'See.': '2020-21',
+    'YOB': 2002,
+    'Address': 'Dhaka',
+    'Institute': 'IST'
+}
+print(studentinfo)
+
+
+#copy dictionary using copy() function
+
+mydict1 = studentinfo.copy()
+print(mydict1)
+
+#copy dictionary using dict keyward
+
+mydict2 = dict(studentinfo)
+print(mydict2)
+
+
